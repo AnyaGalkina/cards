542aa27547b09ef0ceb667e18800cfbaa50ae702
Aisylu24 <firnatovna@mail.ru>
2022-09-20 00:02:05 +0300

    feat(profile-reducer): add profileReducer

--- src/features/auth/authAPI.ts
+++ src/features/auth/authAPI.ts
@@ -1,17 +1,15 @@
-import axios from "axios";
-
+import axios, {AxiosResponse} from "axios";
 
 export const instance = axios.create({
    baseURL: "https://neko-back.herokuapp.com/2.0" || "http://localhost:7542/2.0/",
 });
-
 
 export const authAPI = {
     signUp( payload: {email: string, password: string}) {
         return instance.post("/auth/register", payload);
     },
     updateUser(data: UpdateUserType) {
-        return instance.put<UpdateUserType, ResUpdateUserType>('auth/me', data)
+        return instance.put<UpdateUserType, AxiosResponse<ResUpdateUserType>>('auth/me', data)
     },
     logout(){
         return instance.delete<ResLogoutType>('auth/me')
@@ -44,16 +42,33 @@ export type RegisterResType = {
 
 export type UpdateUserType = {
     name: string,
-    avatar: string
+    avatar: string | null
 }
 
 export type ResUpdateUserType = {
-    updatedUser: UpdateUserType
-    error?: string
+    updatedUser: UserType
+    token: string
+    tokenDeathTime: number
 }
 
 type ResLogoutType = {
     info: string;
     error: string;
+}
+
+export type UserType = {
+    _id: string
+    email: string
+    rememberMe: boolean
+    isAdmin: boolean
+    name: string
+    verified: boolean
+    publicCardPacksCount: number
+    created: string
+    updated: string
+    __v: number
+    token: string
+    tokenDeathTime: number
+    avatar: string | null
 }
 
--- src/features/profile/profile-page/profile-reducer.ts
+++ src/features/profile/profile-page/profile-reducer.ts
@@ -1,16 +1,72 @@
-const initialState = {};
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {authAPI, UpdateUserType, UserType} from "../../auth/authAPI";
+import {setAppErrorAC, setAppStatusAC} from "../../../app/app-reducer";
+import {Dispatch} from "redux";
+import {setIsLoggedInAC} from "../../auth/sign-in/login-reducer";
+import {AppRootState} from "../../../app/store";
+import {AxiosError} from "axios";
+import {errorUtils} from "../../../utils/errorUtils";
 
+const initialState: UserType = {
+    _id: '',
+    email: '',
+    name: '',
+    avatar: null,
+    publicCardPacksCount: 0,
+    created: '',
+    updated: '',
+    isAdmin: false,
+    verified: false,
+    rememberMe: false,
+    __v: 0,
+    token: '',
+    tokenDeathTime: 0
+};
 
-export const profileReducer = (state: InitialStateType = initialState, action: ActionType): InitialStateType => {
-    switch (action.type) {
-        default:
-            return state;
+const slice = createSlice({
+    name: 'profile',
+    initialState: initialState,
+    reducers: {
+        updateUserAC(state, action: PayloadAction<UpdateUserType>) {
+            state = {...state, ...action.payload}
+        }
+    }
+})
+
+export const profileReducer = slice.reducer
+export const {updateUserAC} = slice.actions
+
+export const updateUserTC = (userData: UpdateUserType) => async (dispatch: Dispatch, getState: () => AppRootState) => {
+    try {
+        dispatch(setAppStatusAC('loading'))
+
+        const name = getState().profile.name
+        const avatar = getState().profile.avatar
+
+        let res = await authAPI.updateUser({...userData, name, avatar})
+        updateUserAC(res.data.updatedUser)
+        dispatch(setAppStatusAC('succeeded'))
+    } catch (e) {
+        errorUtils(e as Error | AxiosError<{ error: string }>, dispatch)
+        dispatch(setAppStatusAC('failed'))
     }
 }
 
+export const logoutTC = () => (dispatch: Dispatch) => {
+    dispatch(setAppStatusAC('loading'))
+    authAPI.logout()
+        .then((res) => {
+           // if (res.data.info)
+            dispatch(setIsLoggedInAC(false))
+            dispatch(setAppStatusAC('succeeded'))
+        })
+        .catch((error) => {
+            dispatch(setAppErrorAC(error))
+            dispatch(setAppStatusAC('failed'))
+        })
+}
 
-//TYPES
-export type InitialStateType = typeof initialState;
 
-//mock action
-export type ActionType = {type: "SOME_NAME"};
+
+
+
