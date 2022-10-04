257b8692204b7ca78be208507c3b5cacbe26d81f
AnyaGalkina <anna.blackbird1@gmail.com>
2022-10-04 23:24:05 +0300

    feat: add Learn with questions and answers

--- src/features/cards/CardsHeader/CardsHeader.tsx
+++ src/features/cards/CardsHeader/CardsHeader.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 import {Box, Button, Typography} from "@mui/material";
 import {Create} from "@mui/icons-material";
 import SchoolIcon from "@mui/icons-material/School";
-import {addCardsTC} from "../cards-reducer";
+import {addCardsTC, getCardsTC, setCardsPageCount} from "../cards-reducer";
 import {useAppDispatch} from "../../../common/hooks/useAppDispatch";
 import FadeMenu from "./FadeMenu/FadeMenu";
+import {useNavigate} from "react-router-dom";
+import {ROUTES} from "../../../common/enums/enums";
+import {useAppSelector} from "../../../common/hooks/useAppSelector";
 
 type CardsHeaderPropsType = {
     myProfile: boolean
@@ -13,32 +16,40 @@ type CardsHeaderPropsType = {
 }
 
 const CardsHeader: React.FC<CardsHeaderPropsType> = ({myProfile, cardsPack_id, packName}) => {
-    const dispatch = useAppDispatch()
+    // const cardsTotalCount = useAppSelector(state => state.cards.cardsState.cardsTotalCount);
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
 
     const addCardHandler = () => {
         if (cardsPack_id) {
-            dispatch(addCardsTC({card: {cardsPack_id, question: 'new question', answer: 'new answer'}}))
+            dispatch(addCardsTC({card: {cardsPack_id, question: "new question", answer: "new answer"}}))
         }
     }
 
     const learnHandler = () => {
-        alert('learning')
+        // if (cardsPack_id) {
+        //     dispatch(setCardsPageCount({pageCount: cardsTotalCount}));
+        //     dispatch(getCardsTC(cardsPack_id));
+            navigate(ROUTES.LEARN);
+        // }
     }
 
     const headerBoxStyle = {
-        display: 'flex', justifyContent: 'space-around', margin: '10px'}
+        display: "flex", justifyContent: "space-around", margin: "10px"
+    }
 
     return (
-        <Box >
+        <Box>
             {myProfile ?
                 (<Box sx={headerBoxStyle}>
-                        <Box sx={{display: 'flex',justifyContent: 'space-around'}}>
+                        <Box sx={{display: "flex", justifyContent: "space-around"}}>
                             <Typography variant="h5">
                                 {packName}
                             </Typography>
                             <FadeMenu/>
                         </Box>
-                        <Button variant={'contained'} color={'primary'} onClick={addCardHandler} endIcon={<Create/>}>
+                        <Button variant={"contained"} color={"primary"} onClick={addCardHandler} endIcon={<Create/>}>
                             new card
                         </Button>
                     </Box>
@@ -47,7 +58,7 @@ const CardsHeader: React.FC<CardsHeaderPropsType> = ({myProfile, cardsPack_id, p
                         <Typography variant="h5">
                             {packName}
                         </Typography>
-                        <Button variant={'contained'} color={'primary'} onClick={learnHandler} endIcon={<SchoolIcon/>}>
+                        <Button variant={"contained"} color={"primary"} onClick={learnHandler} endIcon={<SchoolIcon/>}>
                             start learning
                         </Button>
                     </Box>
--- src/features/cards/Learn/Learn.tsx
+++ src/features/cards/Learn/Learn.tsx
@@ -1,27 +1,71 @@
-import React from 'react';
+import React, {useEffect, useState} from "react";
 import {Button, Grid, Paper, Typography} from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import {useAppSelector} from "../../../common/hooks/useAppSelector";
+import {getCard} from "../../../common/utils/cardsSmartRandom";
+import {useAppDispatch} from "../../../common/hooks/useAppDispatch";
+import {ResCardType} from "../cardsAPI";
+import {useNavigate} from "react-router-dom";
+import {ROUTES} from "../../../common/enums/enums";
+import {Answer} from "./answer/Answer";
+import s from "./Learn.module.css";
+import {getCardsTC} from "../cards-reducer";
+import {defaultFilterValues, removeAllFilters} from "../../packs/packs-reducer";
 
 export const Learn = () => {
+    const allCards = useAppSelector(state => state.cards.cardsState.cards);
+    const packName = useAppSelector(state => state.cards.cardsState.packName);
+    const dispatch = useAppDispatch();
+    const [isChecked, setIsChecked] = useState(false);
+    const [card, setCard] = useState<ResCardType>({} as ResCardType);
+    const [cardsToLearn, setCardsToLearn] = useState<ResCardType[]>(allCards);
+    const navigate = useNavigate();
+
     const showAnswerHandler = () => {
+        setIsChecked(true);
     }
 
+    // const onGradeClickHandler = (data: { grade: number, card_id: string }) => {
+    //     dispatch(changeGradeTC(data));
+    // }
+
+    const onNextClickHandler = () => {
+        setCardsToLearn(cardsToLearn.filter(c => c._id !== card?._id));
+        setIsChecked(false);
+    }
+
+    useEffect(() => {
+        setCard(getCard(cardsToLearn));
+    }, []);
+
+    useEffect(() => {
+        if (cardsToLearn.length > 0) {
+            setCard(getCard(cardsToLearn));
+        } else {
+            dispatch(removeAllFilters(defaultFilterValues));
+            navigate(ROUTES.PACKS);
+        }
+    }, [cardsToLearn]);
+
     return (
-        <Grid  container style={{padding: '10px'}} justifyContent={"center"}>
+        <Grid container style={{padding: "10px"}} justifyContent={"center"}>
             <Grid item justifyContent={"center"}>
-            <Button href={'#/cards/pack'} color={'primary'} startIcon={ <ArrowBackIcon/>}>
-                Back to Packs
-            </Button>
-            <Typography>Learn PACK NAME </Typography>
-            <Paper style={{padding: '20px'}}>
-                <Typography>card question </Typography>
-                <Typography>attempts</Typography>
-            <Button onClick={showAnswerHandler} variant={'contained'} color={'primary'} >
-                Show answer
-            </Button>
-                {/*if button was clicked show:*/}
-                <Typography>answer and grades </Typography>
-            </Paper>
+                <Button href={"#/cards/pack"} color={"primary"} startIcon={<ArrowBackIcon/>}>
+                    Back to Packs
+                </Button>
+                <h2>Learn "{packName}"</h2>
+                <Paper style={{padding: "20px"}} className={s.paper}>
+                    <Typography>
+                        <span className={s.subTitle}>Question:</span> {card.question!}
+                    </Typography>
+                    <Typography className={s.shots}>You've already tried {card.shots!} times</Typography>
+                {isChecked
+                    ? <Answer answer={card.answer!} onNextClickHandler={onNextClickHandler}/>
+                    : <Button style={{width: "100%"}} onClick={showAnswerHandler} variant={"contained"} color={"primary"}>
+                        Show answer
+                    </Button>
+                }
+                </Paper>
             </Grid>
         </Grid>
     );
